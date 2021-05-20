673dd4d1bc456d33a7ff7a55bb9e82d36537b562
David Kajpust <kajpustd@gmail.com>
2021-05-20 12:04:40 -0400

    Address Ariels comments from #1

--- contracts/BillingStorage.sol
+++ contracts/BillingStorage.sol
@@ -5,21 +5,9 @@ pragma solidity ^0.8.4;
 import './IBilling.sol';
 
 contract BillingStorage {
-    // -- State --
-
-    // Total amount of GRT we allow to be deposited, for safety, in case we can't get an audit
-    // Set in constructor
-    uint32 public depositThreshold;
-
-    // Unpaid token maximum threshold where Gateway will stop serving queries to the users API key
-    // Set in constructor
-    // Maybe we could do without this. It doesn't do anything in the smart contract. It's just there for informational purposes
-    // BUT - i think it might belong. otherwise we have to store this somewhere in a backend, or in the frontend.
-    uint32 public unpaidTokenMax;
-
     // Matic GRT address
     // Set in constructor
-    address public token;
+    address public immutable token;
 
     // Gateway address
     // Set in constructor
@@ -27,5 +15,4 @@ contract BillingStorage {
 
     // user address --> User Struct
     mapping(address => User) public users;
-
 }
