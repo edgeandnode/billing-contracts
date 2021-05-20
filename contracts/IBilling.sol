1c831d41385da3eecbddfb14b17ef31fa7e70bdc
David Kajpust <kajpustd@gmail.com>
2021-05-20 12:04:40 -0400

    Address Ariels comments from #1

--- contracts/IBilling.sol
+++ contracts/IBilling.sol
@@ -6,66 +6,57 @@ interface IBilling {
 
     struct User {
         uint256 tokensDeposited; // Total tokens deposited under users address
-        uint256 tokensOwed;      // Increased when invoice is created. Decreases when paid. Subgraph will reveal the BAD scenario when owed > deposited
-        mapping(uint256 => Invoice) invoices; // invoiceNumber --> invoiceAmount
-    }
-
-    struct Invoice {
-        bool paid; // true if paid. TODO - thought, do we want to allow for partial payments?
-        uint256 tokensOwed; // Filled in by gateway
+        uint256 tokensOwed;      // Tokens owed. Updated by the gateway
     }
 
     /**
      * @dev User deposits GRT to be pulled by the Gateway to pay an invoice
      */
-    event Deposit(uint256 amount, address user);
+    event Deposit(address indexed user, uint256 amount);
     /**
      * @dev User withdraws funds from the Billing contract
      *      If to == Matic L1 bridge, withdrawToL1() was called
      */
-    event Withdraw(uint256 amount, address user, address to);
+    event Withdraw(address indexed user, address indexed to, uint256 amount);
     /**
-     * @dev Gateway created an invoice for a user to pay 
+     * @dev Gateway updates amount of tokens owed 
      */
-    event InvoiceCreated(uint256 amount, address user, uint256 invoiceNumber);
+    event UpdateTokensOwed(address indexed user, uint256 amount);
     /**
-     * @dev Gateway successfully pulled a deposit to pay for an invoice
+     * @dev Gateway successfully pulled from users deposits
      */
-    event InvoicePaid(uint256 amount, address user, uint256 invoiceNumber);
+    event DepositedPulled(address indexed user, uint256 amount);
 
 
     // -- Configuration --
 
     /**
-     * @dev Set the maximum deposit of GRT each user can submit to the contract
-     * @param _threshold  New threshold being set
+     * @dev Set the new gateway address
+     * @param _newGateway  New gateway address
      */
-    function setDepositThreshold(uint256 _threshold) external;
-
-    function setUnpaidTokenMax() external;
+    function setGateway(address _newGateway) external; // onlyGateway or onlyGovernor, or something
 
     // -- Functions --
 
-    function deposit(uint256 amount) external;
+    function deposit(uint256 _amount) external;
 
     // So anyone can deposit to a user
-    function depositTo(uint256 amount, address to) external;
+    function depositTo(address _to, uint256 _amount) external;
 
-    function withdraw(uint256 amount, address to) external;
+    function withdraw(address _to, uint256 _amount) external;
 
     // I believe it is possible since it can just go straight to the matic bridge and then get tunneled to ETH mainnet contract. Will verify
-    function withdrawToL1(uint256 amount) external;
+    function withdrawToL1(uint256 _amount) external;
 
-    function pullDeposit(address user, uint256 invoiceNumber) external; // onlyGateway modifier
+    function pullDeposit(address _user, uint256 _amount) external; // onlyGateway modifier
 
     // So the gateway can just do 1 tx and close out a lot of invoices. For loop on pullDeposit()
     // Gateway should pre-check the subgraph to not pull on users that would fail
-    function pullDeposits(address[] calldata users, uint256[] calldata invoiceNumbers) external;
+    function pullDeposits(address[] calldata _users, uint256[] calldata _amounts) external;
 
-    // -- Getters -- 
+    // Called by the gateway only
+    function updateOwed(address _user, uint256 _amount) external;
 
-    function getUserInvoice(address user, uint256 invoiceNumber) external view;
-
-
+    function updateManyOwed(address[] calldata _users, uint256[] calldata _amounts) external;
 
 }
