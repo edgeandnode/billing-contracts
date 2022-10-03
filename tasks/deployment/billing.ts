5452f3090a6db75cf49e9403f57aaf21fca27fb0
Pablo Carranza Velez <pablo@edgeandnode.com>
2022-10-03 17:05:06 -0300

    fix: deployed to scratch 1 with some fixes

--- tasks/deployment/billing.ts
+++ tasks/deployment/billing.ts
@@ -17,12 +17,12 @@ task('deploy-billing', 'Deploy the billing contract (use L2 network!)')
   .addParam('collector', 'Address of the collector')
   .addParam('token', 'Address of the token')
   .addParam('governor', 'Address of the governor, leave empty to use the deployer')
-  .addParam('tokenGateway', 'Address of the L2GraphTokenGateway')
+  .addParam('tokengateway', 'Address of the L2GraphTokenGateway')
   .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
     const accounts = await hre.ethers.getSigners()
     const chainId = (hre.network.config.chainId as number).toString()
     const billing = await deployBilling(
-      [taskArgs.collector, taskArgs.token, taskArgs.governor ?? accounts[0].address, taskArgs.tokenGateway],
+      [taskArgs.collector, taskArgs.token, taskArgs.governor ?? accounts[0].address, taskArgs.tokengateway],
       accounts[0] as unknown as Wallet,
     )
     addresses[chainId]['Billing'] = billing.address
@@ -34,21 +34,26 @@ task(
   'Configure the billing contract to set billing connector address and transfer ownership to the governor (use L2 network!)',
 )
   .addParam('governor', 'Address of the governor')
-  .addParam('billingConnector', 'Address of the BillingConnector on L1')
+  .addParam('billingconnector', 'Address of the BillingConnector on L1')
   .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
     const accounts = await hre.ethers.getSigners()
     const chainId = (hre.network.config.chainId as number).toString()
     const addressBook = addresses[chainId]
     const wallet = accounts[0] as unknown as Wallet
-    const billing = getContractAt('Billing', addressBook[chainId]['Billing'], wallet) as unknown as Billing
+    const billing = getContractAt('Billing', addressBook['Billing'], wallet) as unknown as Billing
 
-    logger.log(`Setting L1 billing connector address to ${taskArgs.billingConnector}`)
-    const tx1 = await billing.connect(wallet).setL1BillingConnector(taskArgs.billingConnector)
+    logger.log(`Setting L1 billing connector address to ${taskArgs.billingconnector}`)
+    const tx1 = await billing.connect(wallet).setL1BillingConnector(taskArgs.billingconnector)
     const receipt1 = await tx1.wait()
     logger.log(`> Done. Tx hash: ${receipt1.transactionHash}`)
 
-    logger.log(`Transferring Billing contract ownership to ${taskArgs.governor}`)
-    const tx2 = await billing.connect(wallet).transferOwnership(taskArgs.governor)
-    const receipt2 = await tx2.wait()
-    logger.log(`> Done. Tx hash: ${receipt2.transactionHash}`)
+    const currentBillingGovernor = await billing.governor()
+    if (currentBillingGovernor != taskArgs.governor) {
+      logger.log(`Transferring Billing contract ownership to ${taskArgs.governor}`)
+      const tx2 = await billing.connect(wallet).transferOwnership(taskArgs.governor)
+      const receipt2 = await tx2.wait()
+      logger.log(`> Done. Tx hash: ${receipt2.transactionHash}`)
+    } else {
+      logger.log('Billing is already owned by the right governor, so skipping ownership transfer.')
+    }
   })
--- tasks/deployment/billingConnector.ts
+++ tasks/deployment/billingConnector.ts
@@ -9,15 +9,16 @@ import addresses from '../../addresses.json'
 import { promises as fs } from 'fs'
 
 task('deploy-billing-connector', 'Deploy the billing connector contract (use L1 network!)')
-  .addParam('tokenGateway', 'Address of the L1GraphTokenGateway')
+  .addParam('tokengateway', 'Address of the L1GraphTokenGateway')
   .addParam('billing', 'Address of the Billing contract on L2')
   .addParam('token', 'Address of the token')
   .addParam('governor', 'Address of the governor')
+  .addParam('inbox', 'Address of the Arbitrum DelayedInbox')
   .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
     const accounts = await hre.ethers.getSigners()
     const chainId = (hre.network.config.chainId as number).toString()
     const billingConnector = await deployBillingConnector(
-      [taskArgs.tokenGateway, taskArgs.billing, taskArgs.token, taskArgs.governor],
+      [taskArgs.tokengateway, taskArgs.billing, taskArgs.token, taskArgs.governor, taskArgs.inbox],
       accounts[0] as unknown as Wallet,
     )
     addresses[chainId]['BillingConnector'] = billingConnector.address
