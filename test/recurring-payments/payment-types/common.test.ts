0dc6fb99e5bd549fbb50d233491c14ec71fcd8ca
Tomás Migone <tomas@edgeandnode.com>
2023-09-14 17:20:28 -0300

    fix: switch timekeeping from monthly to time based
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- test/recurring-payments/payment-types/common.test.ts
+++ test/recurring-payments/payment-types/common.test.ts
@@ -43,8 +43,9 @@ describe('RecurringPayments: payment types', () => {
   const tenBillion = toGRT('10000000000')
 
   const initialMaxGasPrice = ethers.utils.parseUnits('3.5', 'gwei')
-  const initialExecutionInterval = 1
-  const initialExpirationInterval = 6
+  const ONE_DAY_IN_SECONDS = 60 * 60 * 24
+  const initialExecutionInterval = 30 * ONE_DAY_IN_SECONDS
+  const initialExpirationInterval = 90 * ONE_DAY_IN_SECONDS
   const tooDamnHighGasPrice = ethers.utils.parseUnits('100', 'gwei')
   const subscriptionsEpochSeconds = BigNumber.from(100)
 
@@ -79,7 +80,7 @@ describe('RecurringPayments: payment types', () => {
     ;[me, governor, gelatoNetwork, user1, collector1, l2TokenGatewayMock] = await getAccounts()
     const now = await latestBlockTimestamp()
     const start = now
-    const end = addMonths(start, 1)
+    const end = addMonths(start, 30 * ONE_DAY_IN_SECONDS)
     const rate = toGRT('5')
     subsData = ethers.utils.defaultAbiCoder.encode(['uint64', 'uint64', 'uint128'], [start, end, rate])
     token = await deployment.deployToken([tenBillion], me.signer, true)
