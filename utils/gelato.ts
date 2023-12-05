05df85fbb8861e59d107c6758db874e0d82c2ea7
Tomás Migone <tomas@edgeandnode.com>
2023-12-05 14:48:39 -0300

    chore: update rp contract to use gelato functions
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- utils/gelato.ts
+++ utils/gelato.ts
@@ -1,19 +1,16 @@
-import hre from 'hardhat'
 import * as deployment from './deploy'
 import { Signer } from 'ethers'
-import * as OpsProxyFactoryArtifact from '../build/artifacts/contracts/tests/OpsProxyFactoryMock.sol/OpsProxyFactoryMock.json'
 
-export async function deployMockGelatoNetwork(deployer: Signer, gelato: string) {
-  // Deploy Gelato Task Treasury
-  const treasury = await deployment.deployTaskTreasuryMock([], deployer, true)
-
-  // Deploy Gelato Automate contract
-  const automate = await deployment.deployAutomateMock([gelato, treasury.address], deployer, true)
+export async function deployMockGelatoNetwork(deployer: Signer) {
+  // Deploy Gelato
+  const gelato = await deployment.deployGelatoMock([], deployer, true)
 
   // Deploy Gelato OpsProxyFactory
-  // The OpsProxyFactory address hardcoded in the Automate contract so we need to copy the bytecode into target address
-  const OPS_PROXY_FACTORY = '0xC815dB16D4be6ddf2685C201937905aBf338F5D7'
-  await hre.network.provider.send('hardhat_setCode', [OPS_PROXY_FACTORY, OpsProxyFactoryArtifact.deployedBytecode])
+  const opsProxyFactory = await deployment.deployOpsProxyFactoryMock([], deployer, true)
 
-  return automate
+  // Deploy Gelato Proxy module contract
+  const proxyModule = await deployment.deployProxyModuleMock([opsProxyFactory.address], deployer, true)
+
+  // Deploy Gelato Automate contract
+  return await deployment.deployAutomateMock([gelato.address, proxyModule.address], deployer, true)
 }
